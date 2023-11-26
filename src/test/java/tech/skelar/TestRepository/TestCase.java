a7d386e87f5428bb5018ad37ad10d3d334002c16
kateryna.arkhanhelska <kateryna.arkhanhelska@alphanovel.io>
2023-11-26 20:59:50 +0200

    -m "My Cat"

---
+++ src/test/java/tech/skelar/TestRepository/TestCase.java
@@ -0,0 +1,12 @@
+package tech.skelar.TestRepository;
+
+import com.github.javafaker.Faker;
+
+public class TestCase {
+    public static void main(String[] args) {
+        var faker = Faker.instance().cat();
+        System.out.println("My cat's \uD83D\uDC31 name is " + faker.name() + " :)");
+        System.out.println("His breed " + faker.breed() + " \uD83D\uDCAB");
+        System.out.println("Its registration " + faker.registry() + " \uD83C\uDF0E");
+    }
+}
